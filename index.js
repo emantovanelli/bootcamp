4e6a9421253fc54c95f0963a11c2f8166e0fe091
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-07-13 20:20:58 -0300

    crud

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /user/1
 // Request body = {"name": Edvaldo, "email": "edvaldo@email.com"}
@@ -27,10 +29,40 @@ const server = express();
 
 const users = ["Edvaldo", "Maria", "Fernando"];
 
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
 
-  return res.send(users[index]);
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return req.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
