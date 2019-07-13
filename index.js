06e44e16280b14ef0cfade87afcdcfd5a61e2ab5
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-07-13 19:21:20 -0300

    query and route params

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.send({ message: "Hello World" });
+// Query params = ?teste=1
+// Route params = /user/1
+// Request body = {"name": Edvaldo, "email": "edvaldo@email.com"}
+
+// Simples
+// server.get("/teste", (req, res) => {
+//   return res.send({ message: "Hello World" });
+// });
+
+// QUery params (http://localhost:3000/teste?nome=Edvaldo)
+// server.get("/teste", (req, res) => {
+//   const nome = req.query.nome;
+
+//   return res.send({ message: `Hello ${nome}` });
+// });
+
+// Route param (localhost:3000/users/123)
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  return res.send({ message: `Buscando o usuário ${id}` });
 });
 
 server.listen(3000);
